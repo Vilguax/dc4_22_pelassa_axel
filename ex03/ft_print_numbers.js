10275b68b0ae8cac0933ab6479f3544007fd2825
Vilguax <axel.pelassa@hotmail.fr>
2023-02-08 20:19:42 +0100

    modif d'une fonction

--- ex03/ft_print_numbers.js
+++ ex03/ft_print_numbers.js
@@ -1,5 +1,5 @@
-function WriteNumbers() {
+function ft_print_numbers() {
     var numbers = "zero, un, deux, trois, quatre, cinq, six, sept, huit, neuf";
     console.log(numbers)
 }
-WriteNumbers();
+ft_print_numbers();
