37a60ef1097db2087cb91dbe080e67553397170b
Vilguax <axel.pelassa@hotmail.fr>
2023-02-08 20:08:54 +0100

    modification de la function WriteNumbers

--- ex03/ ft_print_numbers.js
+++
@@ -1,9 +0,0 @@
-function WriteNumbers() {
-    var numbers = "One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten";
-    var numbersArray = numbers.split(", ");
-    for (var i = 0; i < numbersArray.length; i++) {
-        console.log(numbersArray[i]);
-    }
-}
-
-WriteNumbers();
---
+++ ex03/ft_print_numbers.js
@@ -0,0 +1,5 @@
+function WriteNumbers() {
+    var numbers = "zero, un, deux, trois, quatre, cinq, six, sept, huit, neuf";
+    console.log(numbers)
+}
+WriteNumbers();
