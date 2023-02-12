04ecd974fc61ec36a0efdceb86cc3b589b9587c0
Vilguax <axel.pelassa@hotmail.fr>
2023-02-12 18:52:33 +0100

    ajout des scirpt ex01 - ex03 avec une function digne de ce nom

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -3,3 +3,9 @@ function ft_print_alphabet() {
     console.log(alphabet);
 }
 ft_print_alphabet();
+
+function ft_print_alphabet2() {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    console.log(alphabet.join(''));
+}
+ft_print_alphabet2();
--- ex03/ft_print_numbers.js
+++ ex03/ft_print_numbers.js
@@ -1,5 +1,12 @@
 function ft_print_numbers() {
     var numbers = "zero, un, deux, trois, quatre, cinq, six, sept, huit, neuf";
     console.log(numbers)
+
 }
 ft_print_numbers();
+
+function ft_print_numbers2() {
+    var numbers = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"];
+    console.log(numbers.join(''));
+}
+ft_print_numbers2();
