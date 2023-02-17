93fbabfa87757df240936bff5626a8e2206ba4f1
Vilguax <axel.pelassa@hotmail.fr>
2023-02-17 18:08:44 +0100

    aze

--- ex05/ft_print_comb.js
+++ ex05/ft_print_comb.js
@@ -9,7 +9,7 @@ function ft_print_comb()
         {
             while (t < 9)
             {
-                console.log(n, h, t.join(''));
+                console.log(n, h, t);
                 t++;
             }
             h++;
